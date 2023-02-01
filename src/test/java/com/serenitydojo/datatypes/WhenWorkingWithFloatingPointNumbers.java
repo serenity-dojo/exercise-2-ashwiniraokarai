2317a8bc4de8e90233ea104ce67b1a53c4c9b1b8
Ash <ashwinirao.karai@gmail.com>
2023-02-01 14:15:25 -0700

    completed exercise-2

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
@@ -8,8 +8,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class WhenWorkingWithFloatingPointNumbers {
 
     //
-    // Exercise 1: Convert 27 degrees Celcius to Farenheit
-    // The equation is: farenheit = (9 / 5) * celcius + 32
+    // Exercise 1: Convert 27 degrees Celsius to Fahrenheit
+    // The equation is: fahrenheit = (9 / 5) * celsius + 32
     //
     @Test
     public void convertToFarenheit() {
@@ -17,7 +17,9 @@ public class WhenWorkingWithFloatingPointNumbers {
         double farenheit = 0.0d;
 
         // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
-
+        //Ash: Expressed the elements of division as Doubles instead of integer type suggested in the provided equation. The integers were resulting in a value of 1 instead of 1.8 resulting in
+        //the fahrenheit value to evaluate inaccurately to 59-something instead of the expected value 80.6
+        farenheit = (9.0 / 5.0 * celcius) + 32;
         assertThat(farenheit, equalTo(80.6));
     }
 
@@ -31,8 +33,7 @@ public class WhenWorkingWithFloatingPointNumbers {
         double weightInPounds = 0;
 
         // TODO: Use a floating point calculation to calculate the correct weight in pounds
-
+        weightInPounds = weightInKilograms * 2.20462;
         assertThat(weightInPounds, equalTo(110.231));
-
     }
 }
